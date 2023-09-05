d1a056e1c7364f57d16a46acbcaee34eab077181
yulluone <joshuamuuo5@gmail.com>
2023-09-05 16:28:44 +0300

    first commit

---
+++ pages/_app.jsx
@@ -0,0 +1,85 @@
+import Head from "next/head";
+import { Fragment, useEffect, useState } from "react";
+import Preloader from "../src/layout/Preloader";
+import "../styles/globals.css";
+const App = ({ Component, pageProps }) => {
+  const [loader, setLoader] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false);
+    }, 1000);
+  }, []);
+
+  return (
+    <Fragment>
+      <Head>
+        <title>Aster - Portfolio Agency Template</title>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="HandheldFriendly" content="true" />
+        <meta name="author" content="beshleyua" />
+        {/* Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik+Iso&display=swap"
+          rel="stylesheet"
+        />
+        {/* CSS STYLES */}
+        <link
+          rel="stylesheet"
+          href="assets/css/vendors/bootstrap.css"
+          type="text/css"
+          media="all"
+        />
+        <link
+          rel="stylesheet"
+          href="assets/fonts/font-awesome/css/font-awesome.css"
+          type="text/css"
+          media="all"
+        />
+        <link
+          rel="stylesheet"
+          href="assets/css/vendors/magnific-popup.css"
+          type="text/css"
+          media="all"
+        />
+        <link
+          rel="stylesheet"
+          href="assets/css/vendors/splitting.css"
+          type="text/css"
+          media="all"
+        />
+        <link
+          rel="stylesheet"
+          href="assets/css/vendors/swiper.css"
+          type="text/css"
+          media="all"
+        />
+        <link
+          rel="stylesheet"
+          href="assets/css/vendors/animate.css"
+          type="text/css"
+          media="all"
+        />
+        <link
+          rel="stylesheet"
+          href="assets/css/main.css"
+          type="text/css"
+          media="all"
+        />
+        {/*[if lt IE 9]>
+	
+	
+	<![endif]*/}
+        {/* Favicon */}
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+      {loader && <Preloader />}
+      <Component {...pageProps} />
+    </Fragment>
+  );
+};
+export default App;
